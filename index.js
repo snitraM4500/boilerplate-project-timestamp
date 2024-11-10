22ca885a5932ae21247d1f6e57ee5a132d07fbf0
snitraM4500 <martinsduamba@gmail.com>
2024-11-10 08:02:52 +0200

    feat: the solution

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,36 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/:date", function (req, res) {
+  let number = 0
+  try {
+    number = Number(req.params.date)
+  } catch (error) {
+
+  }
+  let time
+  if (number > 0) {
+    time = new Date(number)
+  } else {
+    time = new Date(req.params.date)
+  }
+  let verifier = time.getDate() + ""
+  if (verifier !== "NaN") {
+    res.json({ "unix": time.getTime(), "utc": `${time.toUTCString()}` });
+  } else if (req.params.date === "") {
+    time = new Date()
+    res.json({ "unix": time.getTime(), "utc": `${time.toUTCString()}` });
+  } else {
+    res.json({ error: "Invalid Date" })
+  }
+});
+
+app.get("/api/", function (req, res) {
+    time = new Date()
+    res.json({ "unix": time.getTime(), "utc": `${time.toUTCString()}` });
 });
 
 
